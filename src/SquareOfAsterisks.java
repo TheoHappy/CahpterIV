9592d6eabf1f0d4d832a4cd8f53164f6ff1da429
TheoHappy <teodore473@gmail.com>
2019-11-29 13:09:22 +0200

    Added 5 classes

---
+++ src/SquareOfAsterisks.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SquareOfASterisks {
+    public static void main(String[] args) {
+        int sizeOfTheSide;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter size of the side of a square :");
+        sizeOfTheSide = input.nextInt();
+        for (int i = 0; i < sizeOfTheSide; i++) {
+            System.out.print("*");
+
+        }
+        System.out.println();
+        for (int i = 0; i < sizeOfTheSide-2; i++) {
+            System.out.print("*");
+            for (int j = 0; j < sizeOfTheSide-2; j++){
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+
+        for (int i = 0; i < sizeOfTheSide; i++) {
+            System.out.print("*");
+        }
+
+    }
+}
